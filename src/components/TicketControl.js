96a69e5cb629e42107c0f2519d3a01b2b6cef152
Jo <joannadawnmiller@gmail.com>
2021-04-21 19:20:44 -0400

    add full CRUD functionality
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
+import EditTicketForm from './EditTicketForm';
 
 class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      masterTicketList: []
+      masterTicketList: [],
+      selectedTicket: null,
+      editing: false
     };
   }
 
   handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+    if (this.state.selectedTicket != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTicket: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleEditingTicketInList = (ticketToEdit) => {
+    const editedMasterTicketList = this.state.masterTicketList
+    .filter(ticket => ticket.id !== this.state.selectedTicket.id)
+    .concat(ticketToEdit);
+    this.setState({
+      masterTicketList: editedMasterTicketList,
+      editing: false,
+      selectedTicket: null
+    });
   }
 
   handleAddingNewTicketToList = (newTicket) => {
@@ -23,14 +50,38 @@ class TicketControl extends React.Component {
                   formVisibleOnPage: false });
   }
 
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.state.masterTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+
+  handleDeletingTicket = (id) => {
+    const newMasterTicketList = this.state.masterTicketList.filter(ticket => ticket.id !== id);
+    this.setState({
+      masterTicketList: newMasterTicketList,
+      selectedTicket: null
+    });
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.editing) {
+      currentlyVisibleState = <EditTicketForm ticket = {this.state.selectedTicket}
+      onEditTicket = {this.handleEditingTicketInList} />
+      buttonText = "Return to Ticket List"
+    } else if (this.state.selectedTicket != null) {
+      currentlyVisibleState =
+      <TicketDetail
+        ticket={this.state.selectedTicket}
+        onClickingDelete={this.handleDeletingTicket}
+        onClickingEdit={this.handleEditClick} />
+      buttonText = "Return to Ticket List"
+    } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}/>
       buttonText = "Return to Ticket List"
     } else {
-      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList}/>
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} onTicketSelection={this.handleChangingSelectedTicket} />
       buttonText = "Add Ticket"
     }
     return (
---
+++ src/components/TicketDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TicketDetail(props) {
+  const { ticket, onClickingDelete } = props;
+  return (
+    <>
+      <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
+      <button onClick={ props.onClickingEdit }>Update Dat Ticket</button>
+      <button onClick={ ()=> onClickingDelete(ticket.id) }>Close Ticket</button>
+      <hr/>
+    </>
+  );
+}
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default TicketDetail;
